a44536ab72d7a2e2cb053e10aeb0073f7d032083
Nimit Patel <49493835+NimitHingrajia@users.noreply.github.com>
2020-01-10 20:37:42 +0530

    Add files via upload

---
+++ dec_to_bin.py
@@ -0,0 +1,13 @@
+def binary(number):
+	s=''
+	bin_val=''
+	while(number>0):
+		s=s+str(number%2)
+		number=number//2
+	
+	if(len(s)<8):
+		s=s+"0"*(8-len(s))
+	
+	for val in s[len(s)-1::-1]:
+		bin_val+=val
+	return bin_val
